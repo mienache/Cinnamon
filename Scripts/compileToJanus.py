dec3a0df37e42eda2ad7457bd25bf22866776884
Mihai Enache <mihai.enache96@gmail.com>
2022-04-09 15:15:00 +0100

    Move thread and IPC specific code to Janus

--- Scripts/compileToJanus.py
+++ Scripts/compileToJanus.py
@@ -129,3 +129,57 @@ with open(fileName+".thread_init", 'r') as thread_init_file:
 			replace_file.write(content_new)
 			print "Janus thread init code:"
 			# print content_new
+with open(fileName+".thread_init", 'r') as thread_init_file:
+	code = thread_init_file.read();
+	code = '/*--- Janus Thread Init Start ---*/\n\n'+code+'\n/*--- Janus Thread Init Finish ---*/'
+	with open(JanusPATH+"/dynamic/dsl/dsl_core.cpp", "r") as dest_file:
+		content = dest_file.read();
+		content_new = re.sub(r'/\*--- Janus Thread Init Start ---\*/.*?/\*--- Janus Thread Init Finish ---\*/', code, content, flags=re.DOTALL)
+		with open(JanusPATH+"/dynamic/dsl/dsl_core.cpp", "w") as replace_file:
+			replace_file.write(content_new)
+			print "Janus thread init code:"
+			# print content_new
+
+with open(fileName+".thread_manager_h", 'r') as thread_manager_h:
+	code = thread_manager_h.read();
+	code = '/*--- Thread Manager Declarations Start ---*/\n\n'+code+'\n/*--- Thread Manager Declarations Finish ---*/'
+	with open(JanusPATH+"/dynamic/dsl/dsl_thread_manager.h", "r") as dest_file:
+		content = dest_file.read();
+		content_new = re.sub(r'/\*--- Thread Manager Declarations Start ---\*/.*?/\*--- Thread Manager Declarations Finish ---\*/', code, content, flags=re.DOTALL)
+		with open(JanusPATH+"/dynamic/dsl/dsl_thread_manager.h", "w") as replace_file:
+			replace_file.write(content_new)
+			print "Janus thread manager h code:"
+			# print content_new
+
+with open(fileName+".thread_manager_cpp", 'r') as thread_manager_cpp:
+	code = thread_manager_cpp.read();
+	code = '/*--- Thread Manager Declarations Start ---*/\n\n'+code+'\n/*--- Thread Manager Declarations Finish ---*/'
+	with open(JanusPATH+"/dynamic/dsl/dsl_thread_manager.cpp", "r") as dest_file:
+		content = dest_file.read();
+		content_new = re.sub(r'/\*--- Thread Manager Declarations Start ---\*/.*?/\*--- Thread Manager Declarations Finish ---\*/', code, content, flags=re.DOTALL)
+		with open(JanusPATH+"/dynamic/dsl/dsl_thread_manager.cpp", "w") as replace_file:
+			replace_file.write(content_new)
+			print "Janus thread manager cpp code:"
+			# print content_new
+
+with open(fileName+".ipc_h", 'r') as ipc_h:
+	code = ipc_h.read();
+	code = '/*--- IPC Declarations Start ---*/\n\n'+code+'\n/*--- IPC Declarations Finish ---*/'
+	with open(JanusPATH+"/dynamic/dsl/dsl_ipc.h", "r") as dest_file:
+		content = dest_file.read();
+		content_new = re.sub(r'/\*--- IPC Declarations Start ---\*/.*?/\*--- IPC Declarations Finish ---\*/', code, content, flags=re.DOTALL)
+		with open(JanusPATH+"/dynamic/dsl/dsl_ipc.h", "w") as replace_file:
+			replace_file.write(content_new)
+			print "Janus IPC h code:"
+			# print content_new
+
+with open(fileName+".ipc_cpp", 'r') as ipc_cpp:
+	code = ipc_cpp.read();
+	code = '/*--- IPC Declarations Start ---*/\n\n'+code+'\n/*--- IPC Declarations Finish ---*/'
+	with open(JanusPATH+"/dynamic/dsl/dsl_ipc.cpp", "r") as dest_file:
+		content = dest_file.read();
+		content_new = re.sub(r'/\*--- IPC Declarations Start ---\*/.*?/\*--- IPC Declarations Finish ---\*/', code, content, flags=re.DOTALL)
+		with open(JanusPATH+"/dynamic/dsl/dsl_ipc.cpp", "w") as replace_file:
+			replace_file.write(content_new)
+			print "Janus IPC cpp code:"
+			# print content_new
